1ab8a490fdd334a7c228114e7b05629f38bfd72c
Francis <francis.antonio0927@gmail.com>
2025-04-06 21:21:44 +0800

    Implement post listing and detail views with proper rendering

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.http import JsonResponse
 # Create your views here.
+
 def post_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    # posts = list(Post.objects.values())
+
+    # return JsonResponse({'posts': posts})
+    return render(request, 'posts/posts_list.html', {'posts' : posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_page.html', {'post' : post})
